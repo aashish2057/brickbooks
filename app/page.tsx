50c121ba7c0136f9a2c216b4bbaaa9b1de6c1e8d
aashish2057 <aashish2057@gmail.com>
2022-12-29 13:21:17 -0600

    add search results display

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import styles from './page.module.css'
+import SearchBar from './SearchBar'
+
+const sets = [
+  {id: "75336", setName: "ATAT"},
+  {id: "75337", setName: "X-Wing"},
+  {id: "75338", setName: "Star Destroyer"},
+  {id: "75339", setName: "Republic Fighter Tank"},
+  {id: "75340", setName: "Republic Gunship"},
+  {id: "75341", setName: "Death Star"},
+  {id: "75342", setName: "ATST"}
+]
 
 export default function Home() {
   return (
     <div>
-      <div className="grid pt-32 place-items-center">
-        <div className="flex text-4xl">
-          <input className="h-14 w-96 bg-neutral-900 outline rounded-lg" type="text" />
-        </div>
-      </div>
+      <SearchBar results={sets} renderItem={(item) => <p>{item.setName}</p>}/>
     </div>
   )
 }
