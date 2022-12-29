4dcec0089824fb9627480c4e1731af8fc070166f
aashish2057 <aashish2057@gmail.com>
2022-12-29 13:24:32 -0600

    add more fake data and extended display height

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,15 @@ const sets = [
   {id: "75339", setName: "Republic Fighter Tank"},
   {id: "75340", setName: "Republic Gunship"},
   {id: "75341", setName: "Death Star"},
-  {id: "75342", setName: "ATST"}
+  {id: "75342", setName: "ATST"},
+  {id: "75343", setName: "Death Star"},
+  {id: "75344", setName: "Gungan Ship"},
+  {id: "75345", setName: "Ashoka Shuttle"},
+  {id: "75346", setName: "Jedi Cruiser"},
+  {id: "75347", setName: "Republic Venator"},
+  {id: "75348", setName: "501st Battlepack"},
+  {id: "75349", setName: "ATTE"},
+  {id: "75350", setName: "Imperial Speeder"},
 ]
 
 export default function Home() {
